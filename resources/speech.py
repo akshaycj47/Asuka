91ada61c4c7217191fbe0447fe449147f6976915
Akshay Sawant <asawant@gatech.edu>
2016-12-17 21:37:45 -0800

    Add speech resources

---
+++ resources/speech.py
@@ -0,0 +1,11 @@
+
+def welcome():
+	return "\
+Welcome to Asuka Console.\n\
+I'm Asuka, I can perform basic tasks based on commands in simple English.\n\
+Some basic commands include: calculate 56 + 72, find a pizza place near New York.\n\
+Type 'quit' to exit."
+
+def cannot_understand():
+	return "\
+Sorry, I didn't undertand your request."
