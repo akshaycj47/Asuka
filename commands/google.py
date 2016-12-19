23d1faa70efd4e8ef4c70ff19451766c1e4bde68
Akshay Sawant <asawant@gatech.edu>
2016-12-18 23:49:13 -0800

    Update google command module

--- commands/google.py
+++ commands/google.py
@@ -1,7 +1,10 @@
 import re
 import webbrowser
 
-def google(query):
-	query = re.findall("\"(.*)\"", query)
-	print "Searching for %s on a new browser window"%(query[0])
-	webbrowser.open("https://www.google.com/search?q=%s"%(query[0]))
+def google(string):
+	try:
+		query = re.findall("\"(.*)\"", string)
+		print "Searching for %s on a new browser window."%(query[0])
+		webbrowser.open("https://www.google.com/search?q=%s"%(query[0]))
+	except:
+		print "I could not understand your request."
