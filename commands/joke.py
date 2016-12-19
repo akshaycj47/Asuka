45cb23eefc55a9bd36ec0c9e44a2a600e396af2e
Akshay Sawant <asawant@gatech.edu>
2016-12-18 23:27:30 -0800

    Update joke module

--- commands/joke.py
+++ commands/joke.py
@@ -1,6 +1,10 @@
 import json
 import unirest
 
+"""
+Function that tells a random joke from a website database
+@param {string} string - Input string from user (not used)
+"""
 def joke(string):
 	try:
 		response = unirest.get("http://tambal.azurewebsites.net/joke/random")
