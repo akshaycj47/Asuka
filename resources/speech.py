0199316a772a10fc3ed5af996ae829a09f1b9419
Akshay Sawant <asawant@gatech.edu>
2016-12-18 19:58:13 -0800

    Finish speech for stock module

--- resources/speech.py
+++ resources/speech.py
@@ -8,7 +8,7 @@ Type 'quit' to exit."
 
 def cannot_understand():
 	return "\
-Sorry, I didn't undertand your request."
+Sorry, I didn't understand your request."
 
 def weather(data):
 	return "\
@@ -18,4 +18,16 @@ Humidity is %s and atmospheric pressure is %s.\n\
 Sunrise is at %s and sunset is at %s (%s)"%(data['city'], data['weather_desc'], data['temp'], data['temp_min'], data['temp_max'], data['humidity'], data['pressure'], data['sunrise'], data['sunset'], data['timezone'])
 
 def stock(data):
-	return ""
+	return "\
+Stock %s:%s has value %s (%s, %s%%)"%(data['stock_exchange'], data['ticker_symbol'], data['stock_value'], data['stock_change'], data['stock_change_pc'])
+
+def stock_incorrect_request():
+	return "\
+Sorry, I didn't understand your request.\n\
+If you want to learn about stock prices, say:\n\
+Please find me stock prices for INTC\n\
+Can you show me stock prices for NYSE:INFY"
+
+def stock_no_info(ticker_symbol):
+	return "\
+I could not find and stock information for %s"%(ticker_symbol)
