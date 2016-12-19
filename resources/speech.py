95ccceb68e24504b028a387d11e3c115d13e93fc
Akshay Sawant <asawant@gatech.edu>
2016-12-18 21:20:34 -0800

    Finish speech for stock module

--- resources/speech.py
+++ resources/speech.py
@@ -17,10 +17,24 @@ Current temperature is %s, with minimum %s and maximum is %s.\n\
 Humidity is %s and atmospheric pressure is %s.\n\
 Sunrise is at %s and sunset is at %s (%s)"%(data['city'], data['weather_desc'], data['temp'], data['temp_min'], data['temp_max'], data['humidity'], data['pressure'], data['sunrise'], data['sunset'], data['timezone'])
 
+"""
+Function that returns response for correct stock request
+@param {object} data - Processed response for stock request
+@param {string} data.stock_exchange - Name of the stock stock_exchange
+@param {string} data.ticker_symbol - Ticker symbol for stock
+@param {string} data.stock_value - Stock value
+@param {string} data.stock_change - Change in stock value (absolute)
+@param {string} data.stock_change_pc - Change in stock value (percentage)
+@return {string} Response
+"""
 def stock(data):
 	return "\
 Stock %s:%s has value %s (%s, %s%%)"%(data['stock_exchange'], data['ticker_symbol'], data['stock_value'], data['stock_change'], data['stock_change_pc'])
 
+"""
+Function that responds to incorrect stock request
+@return {string} Response
+"""
 def stock_incorrect_request():
 	return "\
 Sorry, I didn't understand your request.\n\
@@ -28,6 +42,11 @@ If you want to learn about stock prices, say:\n\
 Please find me stock prices for INTC\n\
 Can you show me stock prices for NYSE:INFY"
 
+"""
+Function that responds to stock request for which there is no information available
+@param {string} ticker_symbol - Ticker symbol for stock_exchange
+@return {string} Response
+"""
 def stock_no_info(ticker_symbol):
 	return "\
 I could not find and stock information for %s"%(ticker_symbol)
