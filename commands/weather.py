1adde043311834a5f710368de21e5687d8ed4be8
Akshay Sawant <asawant@gatech.edu>
2016-12-18 18:01:01 -0800

    Update weather command

--- commands/weather.py
+++ commands/weather.py
@@ -10,47 +10,36 @@ def kelvin_to_fahrenheit(temp):
 	temp = kelvin_to_celsius(temp)
 	return temp * 1.8 + 32
 
-def get_local_timezone(timestamp, lat, lon):
+def get_local_timezone_id(timestamp, lat, lon):
 	#response = unirest.get("https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=AIzaSyAhSNGhwvaBBZdrxcaiUFxGwGjT2a38pzg")
 	response = unirest.get("https://maps.googleapis.com/maps/api/timezone/json?location=%s,%s&timestamp=%s&key=AIzaSyAhSNGhwvaBBZdrxcaiUFxGwGjT2a38pzg"%(lat, lon, timestamp))
-
 	#response = unirest.get("https://maps.googleapis.com/maps/api/timezone/json?location=37.39,-122.08&timestamp=1458000000&key=AIzaSyAhSNGhwvaBBZdrxcaiUFxGwGjT2a38pzg")
-	print response.body
 	return response.body['timeZoneId']
+
+def get_local_timezone_name(timestamp, lat, lon):
+	response = unirest.get("https://maps.googleapis.com/maps/api/timezone/json?location=%s,%s&timestamp=%s&key=AIzaSyAhSNGhwvaBBZdrxcaiUFxGwGjT2a38pzg"%(lat, lon, timestamp))
+	return response.body['timeZoneName']
 
 def unix_to_utc(unix_time):
 	utc_time = datetime.utcfromtimestamp(unix_time)
-	print utc_time
-	print type(utc_time)
 	return utc_time
 
 def unix_to_local_time(unix_time, lat, lon):
-	local_timezone = get_local_timezone(unix_time, lat, lon)
-	local_timezone = tz.gettz(local_timezone)
-	#utc = datetime.strptime('2011-01-21 02:37:21', '%Y-%m-%d %H:%M:%S')
-	#central = utc.astimezone(local_timezone)
+	local_timezone = get_local_timezone_id(unix_time, lat, lon)
+	from_zone      = tz.gettz('UTC')
+	to_zone        = tz.gettz(local_timezone)
 
-
-	from_zone = tz.gettz('UTC')
-	to_zone = tz.gettz('America/New_York')
-
-	# METHOD 2: Auto-detect zones:
-	from_zone = tz.tzutc()
-	to_zone = tz.tzlocal()
-
-	# utc = datetime.utcnow()
-	utc = datetime.strptime('2011-01-21 02:37:21', '%Y-%m-%d %H:%M:%S')
+	utc = datetime.strptime(str(unix_to_utc(unix_time)), '%Y-%m-%d %H:%M:%S')
 
 	# Tell the datetime object that it's in UTC time zone since 
 	# datetime objects are 'naive' by default
 	utc = utc.replace(tzinfo=from_zone)
 
 	# Convert time zone
-	central = utc.astimezone(to_zone)
-	print central
-
-	#print unix_to_utc(unix_time).astimezone(local_timezone)
-	#return unix_to_utc(unix_time).astimezone(local_timezone)
+	local_time = utc.astimezone(to_zone)
+	#print type(local_time.time())
+	#print local_time.time(), local_time.tzinfo
+	return local_time.time()
 
 def process_weather_data(response):
 	data                 = {}
@@ -61,15 +50,15 @@ def process_weather_data(response):
 	data['temp_max']     = "%sC (%sF)"%(kelvin_to_celsius(response.body['main']['temp_max']), kelvin_to_fahrenheit(response.body['main']['temp_max']))
 	data['humidity']     = "%s%%"%(response.body['main']['humidity'])
 	data['pressure']     = "%shPa"%(response.body['main']['pressure'])
-	unix_to_utc(response.body['sys']['sunrise'])
-	unix_to_local_time(response.body['sys']['sunrise'], response.body['coord']['lat'], response.body['coord']['lon'])
+	data['sunrise']      = unix_to_local_time(response.body['sys']['sunrise'], response.body['coord']['lat'], response.body['coord']['lon'])
+	data['sunset']       = unix_to_local_time(response.body['sys']['sunset'], response.body['coord']['lat'], response.body['coord']['lon'])
+	data['timezone']     = get_local_timezone_name(response.body['sys']['sunrise'], response.body['coord']['lat'], response.body['coord']['lon'])
 	return data
 
 def weather(string):
 	response = unirest.get("http://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=873e47c1d02a6432870f10517eb4a38f")
 	#response = unirest.get("http://api.openweathermap.org/data/2.5/weather?id=5349705&appid=873e47c1d02a6432870f10517eb4a38f")
 
-	#print type(response.body)
-	print response.body
+	#print response.body
 	data = process_weather_data(response)
 	print resources.speech.weather(data)
